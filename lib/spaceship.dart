c55de3de82b450c16b59586177318652e5f66dbd
Charles Lee <cwlee1124@gmail.com>
2023-06-26 14:55:42 +0900

    :zap: resize spaceship hitbox size

--- lib/spaceship.dart
+++ lib/spaceship.dart
@@ -34,7 +34,7 @@ class SpaceShip extends SpriteComponent with CollisionCallbacks, KeyboardHandler
         Vector2(0.0, 0.8),
         Vector2(0.0, -0.5),
       ],
-      parentSize: size,
+      parentSize: Vector2.all(32),
     )..renderShape = true);
 
     return super.onLoad();
