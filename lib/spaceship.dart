e80dfcfa48d204ad05625d7964c5ac35be1b3b30
Charles Lee <cwlee1124@gmail.com>
2023-06-26 14:50:20 +0900

    :pencil2: Fix typos.

--- lib/spaceship.dart
+++ lib/spaceship.dart
@@ -32,7 +32,7 @@ class SpaceShip extends SpriteComponent with CollisionCallbacks, KeyboardHandler
         Vector2(0.0, 0.7),
         Vector2(-0.8, 0.0),
         Vector2(0.0, 0.8),
-        Vector2(0.0, -0.5),\
+        Vector2(0.0, -0.5),
       ],
       parentSize: size,
     )..renderShape = true);
